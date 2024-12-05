7569a80a7c488a55b70d094f6197139360d34706
Shoaibalirts <shoaibali.rts@gmail.com>
2024-12-05 18:04:52 +0100

    forwarding input event to the function

--- Documents/React Projects/form-project/src/components/Login.jsx
+++ Documents/React Projects/form-project/src/components/Login.jsx
@@ -1,32 +1,57 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    identifier: "",
+    password: "",
+  });
+
+  function handleInputChange(element, event) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [element]: event.target.value,
+    }));
+  }
+
   function handleSubmission(e) {
     e.preventDefault();
     console.log("submitted");
-    // console.log(myForm);
+    //console.log("Entered Identifier: ", enteredValues.identifier);
+    //console.log("Entered Password: ", enteredValues.password);
+    console.log(enteredValues);
   }
   return (
     <>
-
       <form name="myForm" onSubmit={handleSubmission}>
         <h2>Login</h2>
 
         <div className="control-row">
           <div className="control no-margin">
             <label htmlFor="email">Email</label>
-            <input id="email" type="email" name="identifier" />
+            <input
+              id="identifier"
+              type="identifier"
+              name="identifier"
+              onChange={(event) => handleInputChange("identifier", event)}
+              value={enteredValues.identifier}
+            />
           </div>
 
           <div className="control no-margin">
             <label htmlFor="password">Password</label>
-            <input id="password" type="password" name="password" />
+            <input
+              id="password"
+              type="password"
+              name="password"
+              onChange={(event) => handleInputChange("password", event)}
+              value={enteredValues.password}
+            />
           </div>
         </div>
 
         <p className="form-actions">
           <button className="button button-flat">Reset</button>
-          <button className="button">
-            Login
-          </button>
+          <button className="button">Login</button>
         </p>
       </form>
     </>
